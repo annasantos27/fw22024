bee54ff8884e07b582898c331e1fdd4c8595caeb
annasantos27 <120032731+annasantos27@users.noreply.github.com>
2024-10-07 14:16:19 +0000

    aula 07/10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,20 @@
 import Footer from "./modulos/footer";
-import Navbar from "./modulos/navbar";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Menu from "./modulos/navbar";
+import {Container, Row, Col} from 'react-bootstrap';
 
 export default function index() {
   return<div>
-    <Navbar />
+    <Menu />
     Bem vindos
     <Container>
       <Row>
-        <Col className="bg-primary-subtle"> 1 of 2</Col>
-        <Col className="bg-secondary">2" of 2</Col>
+        <Col className="bg-primary"> 1 of 2</Col>
+        <Col className="bg-primary-subtle">2 of 2</Col>
       </Row>
       <Row>
-        <Col className="bg-secondary-subtle">1 of 3</Col>
-        <Col className="bg-success">2 of 3</Col>
-        <Col className="bg-success-subtle">"3 of 3</Col>
+        <Col className="bg-primary">1 of 3</Col>
+        <Col className="bg-primary-subtle">2 of 3</Col>
+        <Col className="bg-primary">3 of 3</Col>
       </Row>
     </Container>
     <Footer />
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,19 @@
-function Navbar() {
-    return<div>Menu</div>    
+import {Container, Nav, Navbar, NavDropdown}from 'react-bootstrap';
+
+export default function Menu() {
+  return (
+    <Navbar expand="sm" className="bg-primary-subtle">
+      <Container>
+        <Navbar.Brand href="#home">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Produtos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
+
