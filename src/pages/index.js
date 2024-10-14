7df1a9dcf78175c8bb56d2c70574a16735f9f7f1
annasantos27 <120032731+annasantos27@users.noreply.github.com>
2024-10-14 14:21:35 +0000

    aula 14-10

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,11 @@ export default function index() {
     <Container>
       <Row>
         <Col className="bg-primary"> 1 of 2</Col>
-        <Col className="bg-primary-subtle">2 of 2</Col>
+        <Col className="bg-primary">2 of 2</Col>
       </Row>
       <Row>
         <Col className="bg-primary">1 of 3</Col>
-        <Col className="bg-primary-subtle">2 of 3</Col>
+        <Col className="bg-primary">2 of 3</Col>
         <Col className="bg-primary">3 of 3</Col>
       </Row>
     </Container>
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,19 +1,21 @@
 import {Container, Nav, Navbar, NavDropdown}from 'react-bootstrap';
-
+import styles from "@/styles/navbar.module.css"
 export default function Menu() {
-  return (
-    <Navbar expand="sm" className="bg-primary-subtle">
-      <Container>
+  return ( 
+  <Container>
+    <Navbar expand="sm" className={`${styles.navgradiente} bg-primary-subtle px-0 my-0`}>
+     
         <Navbar.Brand href="#home">6A</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#link">Produtos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      
     </Navbar>
+    </Container>
   );
 }
 
